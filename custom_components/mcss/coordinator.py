48c7506917aa5948899ce932d1915383c73a79cc
Barrel0Memes97 <commanderzander08@gmail.com>
2026-09-14 14:36:09 -0500

    V3.0.1-dev Fixing options menu format

--- custom_components/mcss/coordinator.py
+++ custom_components/mcss/coordinator.py
@@ -148,8 +148,8 @@ class MCSSCoordinator(DataUpdateCoordinator[dict[str, dict]]):
         self.player_seconds = int(
             options.get(
                 CONF_PLAYER_INTERVAL,
-    CONF_FILTER_PLAYER_MESSAGES,
-    DEFAULT_FILTER_PLAYER_MESSAGES,
+                CONF_FILTER_PLAYER_MESSAGES,
+                DEFAULT_FILTER_PLAYER_MESSAGES,
                 DEFAULT_PLAYER_INTERVAL,
             )
         )
